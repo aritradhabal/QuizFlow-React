0e8a69a0008b5d5a612bd404f679d93bdd74b88c
Aritra Dhabal <aritra@mac.local>
2025-07-29 07:12:28 +0530

    ui-changes

--- src/components/sidebar/Sideitems.jsx
+++ src/components/sidebar/Sideitems.jsx
@@ -23,15 +23,15 @@ function Sideitems({SideBar, SetSideBar}) {
   return (
     <>
         <div className="flex flex-col justify-center gap-y-2 mt-10 px-5">
-            <button onClick={create} className={`Create cursor-pointer flex items-center justify-start transition duration-350 gap-x-2 rounded-sm hover:bg-[#E2E8F0] py-1 px-2 ${SideBar === 'Create' ? 'bg-[#FDEDF7]' : ''}`}>
+            <button onClick={create} className={`Create cursor-pointer flex items-center justify-start transition duration-350 gap-x-2 rounded-sm hover:bg-[#E2E8F0] py-1 px-2 ${SideBar === 'Create' ? 'bg-[#FDEDF7] hover:bg-[#FDEDF7]' : ''}`}>
                 {SideBar === 'Create' ? <AiFillThunderbolt className='fill-pink-500'/> : <AiFillThunderbolt className='fill-gray-700'/>}
                 <p className='font-medium'>Create</p>
             </button>
-            <button onClick={activity} className={`Create cursor-pointer flex items-center justify-start transition duration-350 gap-x-2 rounded-sm hover:bg-[#E2E8F0] py-1 px-2 ${SideBar === 'Activity' ? 'bg-[#FDEDF7]' : ''}`}>
+            <button onClick={activity} className={`Create cursor-pointer flex items-center justify-start transition duration-350 gap-x-2 rounded-sm hover:bg-[#E2E8F0] py-1 px-2 ${SideBar === 'Activity' ? 'bg-[#FDEDF7] hover:bg-[#FDEDF7]' : ''}`}>
                 {SideBar === 'Activity' ? <HiDocumentSearch className='fill-pink-500'/> : <HiDocumentSearch  className=''/>}
                 <p className='font-medium'>Activity</p>
             </button>
-            <button onClick={join} className={`Create cursor-pointer flex items-center justify-start transition duration-350 gap-x-2 rounded-sm hover:bg-[#E2E8F0] py-1 px-2 ${SideBar === 'Join' ? 'bg-[#FDEDF7]' : ''}`}>
+            <button onClick={join} className={`Create cursor-pointer flex items-center justify-start transition duration-350 gap-x-2 rounded-sm hover:bg-[#E2E8F0] py-1 px-2 ${SideBar === 'Join' ? 'bg-[#FDEDF7] hover:bg-[#FDEDF7]' : ''}`}>
                 {SideBar === 'Join' ? <RiQuillPenFill className='fill-pink-500'/> : <RiQuillPenFill className=''/>}
                 <p className='font-medium'>Join</p>
             </button>
